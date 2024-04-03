03cd61b305160b1c18db5c7d8dd24269b1c7944c
samirkarimov022 <samir.karimov0022@gmail.com>
2024-04-03 06:18:55 +0400

    task add

---
+++ ConsoleApp1/Models/Weapon.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    internal class Weapon
+    {
+        private int bulletCapacity;
+        private int bulletCount;
+        private int fireRate;
+        private int reloadTime;
+        private string shootingMode;
+
+        public int BulletCapacity
+        {
+            get { return bulletCapacity; }
+            set { bulletCapacity = value; }
+        }
+
+        public int BulletCount
+        {
+            get { return bulletCount; }
+            private set { bulletCount = value; }
+        }
+
+        public int FireRate
+        {
+            get { return fireRate; }
+            set { fireRate = value; }
+        }
+
+        public int ReloadTime
+        {
+            get { return reloadTime; }
+            set { reloadTime = value; }
+        }
+
+        public string ShootingMode
+        {
+            get { return shootingMode; }
+            set { shootingMode = value; }
+        }
+
+        public Weapon(int bulletCapacity, int fireRate, int reloadTime, string shootingMode)
+        {
+            BulletCapacity = bulletCapacity;
+            BulletCount = bulletCapacity;
+            FireRate = fireRate;
+            ReloadTime = reloadTime;
+            ShootingMode = shootingMode;
+        }
+
+        public void Shoot()
+        {
+            if (BulletCount > 0)
+            {
+                Console.WriteLine("Gülle atıldı.");
+                BulletCount--;
+            }
+            else
+            {
+                Console.WriteLine("Daraqda gülle yoxdur.");
+            }
+        }
+
+        public void Fire()
+        {
+            if (BulletCount > 0)
+            {
+                double timeToFinish = (double)BulletCount / FireRate;
+                Console.WriteLine($"Təxmini {timeToFinish:F2} saniyədə bitəcək.");
+                BulletCount = 0;
+            }
+            else
+            {
+                Console.WriteLine("Daraqda gülle yoxdur.");
+            }
+        }
+
+        public int GetRemainBulletCount()
+        {
+            return BulletCapacity - BulletCount;
+        }
+
+        public void Reload()
+        {
+            Console.WriteLine("Daraq yenidən doldurulur...");
+            BulletCount = BulletCapacity;
+        }
+
+        public void ChangeFireMode()
+        {
+            if (ShootingMode == "tekli")
+            {
+                ShootingMode = "avtomatik";
+            }
+            else
+            {
+                ShootingMode = "tekli";
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using ConsoleApp1.Models;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon weapon = new Weapon(bulletCapacity: 30, fireRate: 5, reloadTime: 3, shootingMode: "tekli");
+
+            while (true)
+            {
+                Console.WriteLine("\n0 - İnformasiya almaq üçün");
+                Console.WriteLine("1 - Shoot metodu üçün");
+                Console.WriteLine("2 - Fire metodu üçün");
+                Console.WriteLine("3 - GetRemainBulletCount metodu üçün");
+                Console.WriteLine("4 - Reload metodu üçün");
+                Console.WriteLine("5 - ChangeFireMode metodu üçün");
+                Console.WriteLine("6 - Proqrami dayandırmaq üçün");
+
+                Console.Write("Seçiminizi daxil edin: ");
+                string choice = Console.ReadLine();
+                Console.Clear();
+                switch (choice)
+                {
+                    case "0":
+                        Console.WriteLine($"Daraqın boşalma saniyəsi: {weapon.ReloadTime}");
+                        Console.WriteLine($"Daraqdakı güllə sayı: {weapon.GetRemainBulletCount()}");
+                        Console.WriteLine($"Darağın güllə tutumu: {weapon.BulletCapacity}");
+                        Console.WriteLine($"Atış modu: {weapon.ShootingMode}");
+                        break;
+                    case "1":
+                        weapon.Shoot();
+                        break;
+                    case "2":
+                        weapon.Fire();
+                        break;
+                    case "3":
+                        Console.WriteLine($"Daraqdakı güllə sayı: {weapon.GetRemainBulletCount()}");
+                        break;
+                    case "4":
+                        weapon.Reload();
+                        break;
+                    case "5":
+                        weapon.ChangeFireMode();
+                        Console.WriteLine("Atış modu dəyişdirildi.");
+                        break;
+                    case "6":
+                     
+                        return;
+                    default:
+                        Console.WriteLine("Düzgün seçim daxil edilməyib.");
+                        break;
+                }
+            }
+        }
+    }
+}
